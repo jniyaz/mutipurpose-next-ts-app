3328a4ee22441b5e7b47e949448e6737bb9d94c0
Niyaz <niyaz.jm@icarasia.com>
2024-02-03 20:48:45 +0800

    add app navbar, card skeletons and updating global styles

---
+++ src/components/Skeletons/HorzCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HorzCard = () => {
+  return (
+    <div className="max-w-full p-4 m-10 mx-auto border border-gray-300 rounded-md shadow max-h-md">
+      <div className="flex space-x-4 animate-pulse">
+        <div className="flex-1 py-1 space-y-4">
+          <div className="w-full h-40 bg-gray-400 rounded"></div>
+          <div className="space-y-2">
+            <div className="h-4 bg-gray-400 rounded"></div>
+            <div className="w-5/6 h-4 bg-gray-400 rounded"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HorzCard;
