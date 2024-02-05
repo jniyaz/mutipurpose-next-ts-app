62d5722540ecb8d3d5c0f1e35321f4c20f81b8e1
Niyaz <niyaz.jm@icarasia.com>
2024-02-05 23:30:54 +0800

    fix test scenario packages and vitest configs

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,15 @@
-//...
+//<reference types="vitest" />
 import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     global: true,
     environment: "jsdom",
+    css: true,
+    // setupFiles: "./src/tests/setup.ts",
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
